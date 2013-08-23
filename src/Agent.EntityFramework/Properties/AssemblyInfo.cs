cc915dbee1031584f59175cbf6f058ea8365f12a
Kendall Miller <kendall@gibraltarsoftware.com>
2013-08-23 00:54:59 -0400

    Updated for EF 6 RC 1

--- src/Agent.EntityFramework/Properties/AssemblyInfo.cs
+++ src/Agent.EntityFramework/Properties/AssemblyInfo.cs
@@ -51,4 +51,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.5.2.0")]
-[assembly: AssemblyFileVersion("3.5.2.1521")] //releases are sych'ed up to Loupe build numbers
+[assembly: AssemblyFileVersion("3.5.3.1533")] //releases are sych'ed up to Loupe build numbers
