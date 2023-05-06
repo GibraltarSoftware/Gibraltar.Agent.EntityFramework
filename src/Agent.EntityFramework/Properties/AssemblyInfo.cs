ce6c2aa8ee3b295add2504bc54f2c160e61b4f26
Kendall Miller <kendall@gibraltarsoftware.com>
2023-05-05 22:07:37 -0400

    Update to Gibraltar.Agent v5

--- src/Agent.EntityFramework/Properties/AssemblyInfo.cs
+++ src/Agent.EntityFramework/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gibraltar Software, Inc.")]
 [assembly: AssemblyProduct("Gibraltar.Agent.EntityFramework")]
-[assembly: AssemblyCopyright("Copyright © 2013-2018 Gibraltar Software, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2013-2023 Gibraltar Software, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,6 +50,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.6.4.0")] //releases are sych'ed up to Loupe build numbers
-[assembly: AssemblyInformationalVersion("4.6.4.0")] //releases are sych'ed up to Loupe build numbers
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyInformationalVersion("5.0.0.0")]
